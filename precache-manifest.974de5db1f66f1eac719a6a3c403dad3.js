98fbbdedcfd3b42ad8bc960139f932279001f69f
Shelsea Saint-Fleur <shelsea@bamboosoft.ca>
2018-11-10 10:20:53 -0500

    Updates

---
+++ precache-manifest.974de5db1f66f1eac719a6a3c403dad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d15d34c58228292048b",
+    "url": "/LOG515_FrontEnd/static/css/main.86ef6037.chunk.css"
+  },
+  {
+    "revision": "4d15d34c58228292048b",
+    "url": "/LOG515_FrontEnd/static/js/main.4d15d34c.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/LOG515_FrontEnd/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "4dd3f1cfe512dfbc6e39",
+    "url": "/LOG515_FrontEnd/static/js/runtime~main.4dd3f1cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LOG515_FrontEnd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "95d56535ab8a456d5938305c5b0305f0",
+    "url": "/LOG515_FrontEnd/index.html"
+  }
+];
