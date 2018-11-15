66a2c5d926e7802d7e7b9d21a74fe9173df525cb
Kevin Duvignau <kevin.duvignau.1@ens.etsmtl.ca>
2018-11-15 17:34:27 -0500

    Updates

---
+++ precache-manifest.cf31beb410b939f6fea9e547993e2856.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/LOG515_FrontEnd/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "4dd3f1cfe512dfbc6e39",
+    "url": "/LOG515_FrontEnd/static/js/runtime~main.4dd3f1cf.js"
+  },
+  {
+    "revision": "6e1caf0ca1fda0d4fc0c",
+    "url": "/LOG515_FrontEnd/static/js/main.6e1caf0c.chunk.js"
+  },
+  {
+    "revision": "53812ef4445651c02c2b",
+    "url": "/LOG515_FrontEnd/static/js/1.53812ef4.chunk.js"
+  },
+  {
+    "revision": "6e1caf0ca1fda0d4fc0c",
+    "url": "/LOG515_FrontEnd/static/css/main.da38a188.chunk.css"
+  },
+  {
+    "revision": "53812ef4445651c02c2b",
+    "url": "/LOG515_FrontEnd/static/css/1.20e3bad2.chunk.css"
+  },
+  {
+    "revision": "ab0c0e7310be7cf4d3b8a5a2e1f1d406",
+    "url": "/LOG515_FrontEnd/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3bb25245bc7f8cc3224ffeb8ce91798.js"
+  "/LOG515_FrontEnd/precache-manifest.cf31beb410b939f6fea9e547993e2856.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/LOG515_FrontEnd/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
