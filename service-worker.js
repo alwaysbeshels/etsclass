bd0b822b96f2125cb7839da9e100c21f1ecebd0c
Kevin Duvignau <kevin.duvignau.1@ens.etsmtl.ca>
2018-11-15 17:28:38 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/LOG515_FrontEnd/precache-manifest.2fba6b3c728c384ec56b6b398931acf4.js"
+  "/LOG515_FrontEnd/precache-manifest.cf31beb410b939f6fea9e547993e2856.js"
 );
 
 workbox.clientsClaim();
