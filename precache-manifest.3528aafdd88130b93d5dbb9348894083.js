3b64f7a449aca77f7ef5ee06e5a9118ca59af176
Shelsea Saint-Fleur <shelsea@bamboosoft.ca>
2019-01-16 10:42:13 -0500

    Updates

---
+++ precache-manifest.3528aafdd88130b93d5dbb9348894083.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b46afdddfb5ed3111729",
+    "url": "/classets/static/css/main.eeea2c41.chunk.css"
+  },
+  {
+    "revision": "b46afdddfb5ed3111729",
+    "url": "/classets/static/js/main.b46afddd.chunk.js"
+  },
+  {
+    "revision": "fc6555f8c6e2f7e90c69",
+    "url": "/classets/static/css/1.fba702d9.chunk.css"
+  },
+  {
+    "revision": "fc6555f8c6e2f7e90c69",
+    "url": "/classets/static/js/1.fc6555f8.chunk.js"
+  },
+  {
+    "revision": "ee5821d64a3d9ddc4805",
+    "url": "/classets/static/js/runtime~main.ee5821d6.js"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/classets/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "019e6b4bdca4e3d823f006fca64b8415",
+    "url": "/classets/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/classets/precache-manifest.6ffac5b6a55a459778e9941d998f8212.js"
+  "/classets/precache-manifest.3528aafdd88130b93d5dbb9348894083.js"
 );
 
 workbox.clientsClaim();
