e0f985cefc0793d2e4c2faa2c1b6847672a3b3cb
Shelsea Saint-Fleur <shelsea@bamboosoft.ca>
2019-02-24 21:04:11 -0500

    Updates

---
+++ precache-manifest.7dab88967131598b6f75e0fb5e4aaf86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "55ab1e48a3c5479a5086",
+    "url": "/etsclass/static/css/main.0b749c67.chunk.css"
+  },
+  {
+    "revision": "55ab1e48a3c5479a5086",
+    "url": "/etsclass/static/js/main.55ab1e48.chunk.js"
+  },
+  {
+    "revision": "4228d08968387bb34bad",
+    "url": "/etsclass/static/js/runtime~main.4228d089.js"
+  },
+  {
+    "revision": "966cd3ce0bf920daa0d0",
+    "url": "/etsclass/static/css/2.e13c065f.chunk.css"
+  },
+  {
+    "revision": "966cd3ce0bf920daa0d0",
+    "url": "/etsclass/static/js/2.966cd3ce.chunk.js"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/etsclass/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "7bd8b34d8c52548b7c1b0fe25e03afa3",
+    "url": "/etsclass/static/media/class_ets.7bd8b34d.png"
+  },
+  {
+    "revision": "23ab06a86b624a07cd77bc2a65442f51",
+    "url": "/etsclass/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/classets/precache-manifest.c4e79cfecfc7bd858a229233ba6369fc.js"
+  "/etsclass/precache-manifest.7dab88967131598b6f75e0fb5e4aaf86.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/classets/index.html", {
+workbox.routing.registerNavigationRoute("/etsclass/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
